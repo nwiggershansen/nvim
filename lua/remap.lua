52c1069875d1318a5419660913a3e627bcd21907
KaptajnenGaming <Wiggers_@hotmail.com>
2022-11-07 07:36:57 +0100

    Add CTRL+P as a fuzzy finder alternative

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,7 +2,7 @@ local builtin = require('telescope.builtin')
 local nnoremap = require("keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
-
+nnoremap("<C-p>", builtin.find_files)
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
