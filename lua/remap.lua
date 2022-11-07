fbb2d0323d81bc93bc7ba75ff3030f2d5e494c16
KaptajnenGaming <Wiggers_@hotmail.com>
2022-11-07 07:28:20 +0100

    Remap Ctrl-d and Ctrl-U to also center the cursor on the screen

--- lua/remap.lua
+++ lua/remap.lua
@@ -9,3 +9,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+
