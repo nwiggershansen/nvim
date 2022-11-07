0695bad002928a2a24f6d7d48aaffbb7a40f80fa
KaptajnenGaming <Wiggers_@hotmail.com>
2022-11-07 09:34:12 +0100

    Allow selecting the suggestions using <CR>

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,14 +1,17 @@
 local builtin = require('telescope.builtin')
 local nnoremap = require("keymap").nnoremap
+local inoremap = require("keymap").inoremap
+local EXPR_NOERR = {expr = true, silent = true, nowait = true }
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("<C-p>", builtin.find_files)
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+nnoremap('<leader>ff', builtin.find_files)
+nnoremap('<leader>fg', builtin.live_grep)
+nnoremap('<leader>fb', builtin.buffers)
+nnoremap('<leader>fh', builtin.help_tags)
 
 nnoremap("<C-u>", "<C-u>zz")
 nnoremap("<C-d>", "<C-d>zz")
+
+inoremap('<CR>', 'pumvisible() ?  coc#_select_confirm() : "<CR>"', EXPR_NOERR)
 
