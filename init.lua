c36de82fd9bb0a5127d0655dc25608de6277c737
KaptajnenGaming <Wiggers_@hotmail.com>
2022-11-07 15:29:41 +0100

    Ale linting errors less intrusive icon

--- init.lua
+++ init.lua
@@ -9,3 +9,10 @@ vim.opt.wildignore:append { '*.meta' }
 vim.g.ale_linters = {
     cs = { "OmniSharp" }
 }
+
+vim.g.ale_sign_error = '•'
+vim.g.ale_sign_warning = '•'
+vim.g.ale_sign_info = '·'
+vim.g.ale_sign_style_error = '·'
+vim.g.ale_sign_stylle_warning = '·'
+
