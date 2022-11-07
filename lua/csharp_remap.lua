0aeec151b6df99a4962526c120f7ce7d4bb459d4
KaptajnenGaming <Wiggers_@hotmail.com>
2022-11-07 20:22:04 +0100

    Add format command motion

--- lua/csharp_remap.lua
+++ lua/csharp_remap.lua
@@ -3,4 +3,5 @@ local SILENT = { silent = true }
 
 nnoremap('<F2>', ':OmniSharpRename<CR>', SILENT)
 nnoremap('<C-M>', ':OmniSharpGetCodeActions<CR>', SILENT)
+nnoremap('<C-F>', ':OmniSharpCodeFormat<CR>', SILENT)
 
