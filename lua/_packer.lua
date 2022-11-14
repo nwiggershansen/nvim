a0cb0c1ae482a473796702ce47bdc56b0263d315
KaptajnenGaming <Wiggers_@hotmail.com>
2022-11-14 09:58:26 +0100

    Formatting packer file, with more information, add nerdcommenter

--- lua/_packer.lua
+++ lua/_packer.lua
@@ -5,27 +5,42 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Themes
-  use 'folke/tokyonight.nvim'
   use 'vim-airline/vim-airline-themes'
   use 'vim-airline/vim-airline'
 
   --  use 'KaptajnenGaming/themelas.nvim'
   --  use 'H:/workspace/themelas'
 
+  -- Personal theme
   use 'H:/workspace/themelas'
 
   -- All of it
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-  use 'dense-analysis/ale'
-  use 'tpope/vim-fugitive'
   use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/plenary.nvim'
+  use 'dense-analysis/ale'
+
+  -- Fuzzy finder
+  use 'nvim-telescope/telescope.nvim'
+
+  -- LSP installer
   use 'williamboman/mason.nvim'
-  use 'OmniSharp/omnisharp-vim'
+
+  -- Treesitter
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+  use 'nvim-treesitter/playground'
+
+  -- Auto completion
   use 'hrsh7th/nvim-cmp'
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/cmp-nvim-lsp'
-  use 'nvim-treesitter/playground'
+
+  -- Comment helper
+  use 'preservim/nerdcommenter'
+
+  -- Git
+  use 'tpope/vim-fugitive'
+
+  -- C# specific
+  use 'OmniSharp/omnisharp-vim'
 end)
