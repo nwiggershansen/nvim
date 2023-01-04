501913e3007afbd2113ecf017122adcef8ec62f0
nwiggershansen <wiggers_+nwiggershansen@hotmail.com>
2023-01-04 15:31:41 +0100

    Add a sumneko os check to determine correct lsp placement

--- after/plugin/lsp/sumneko.lua
+++ after/plugin/lsp/sumneko.lua
@@ -1,6 +1,15 @@
 local base_config = require('user.base_config')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local sumneko_path = base_config.mason_path .. "lua-language-server/extension/server/bin/lua-language-server.exe"
+
+local sumneko_path = base_config.mason_path
+if vim.fn.has "mac" == 1 then
+elseif vim.fn.has "unix" == 1 then
+  sumneko_path = sumneko_path .. "bin/lua-language-server"
+elseif vim.fn.has "win32" == 1 then
+  sumneko_path = sumneko_path .. "lua-language-server/extension/server/bin/lua-language-server.exe"
+else
+  return
+end
 
 require('lspconfig').sumneko_lua.setup({
   on_attach    = function()
