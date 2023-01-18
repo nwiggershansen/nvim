93624127789a06bfd26170ff9175eeda531ecb37
KaptajnenGaming <Wiggers_@hotmail.com>
2023-01-18 19:49:12 +0100

    Don't use remap, use keymap set directly - Add vim.lsp.buf.hover()

--- lua/user/base_config.lua
+++ lua/user/base_config.lua
@@ -1,15 +1,17 @@
-local nnoremap = require('keymap').nnoremap
-
 local M = {}
+
 local base_keymaps = function()
-  nnoremap("gd", function() vim.lsp.buf.definition() end)
-  nnoremap("<F2>", function() vim.lsp.buf.rename() end)
-  nnoremap("[d", function() vim.diagnostic.goto_next() end)
-  nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-  nnoremap("<C-m>", function() vim.lsp.buf.code_action() end)
-  nnoremap("<C-f>", function() vim.lsp.buf.format { async = true } end)
-  nnoremap('<C-k>', function() vim.lsp.buf.signature_help() end)
-  nnoremap('<C-n>', function() vim.lsp.buf.references() end)
+  local opts = { remap = false }
+
+  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set('n', '<C-m>', function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set('n', '<C-f>', function() vim.lsp.buf.format { async = true } end, opts)
+  vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set('n', '<C-n>', function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.hover() end, opts)
 end
 
 M.keymap = base_keymaps
@@ -28,4 +30,3 @@ else
 end
 
 return M
-
