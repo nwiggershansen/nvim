8c623893cabd0f6d0e0359d216e1aa8c2ee2eb12
KaptajnenGaming <Wiggers_@hotmail.com>
2023-03-21 20:01:48 +0100

    Rename Sumneko to lua_ls

--- after/plugin/lsp/lua_ls.lua
+++ after/plugin/lsp/lua_ls.lua
@@ -1,21 +1,21 @@
 local base_config = require('user.base_config')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local sumneko_path = base_config.mason_path
+local lua_bin_path = base_config.mason_path
 if vim.fn.has "mac" == 1 then
 elseif vim.fn.has "unix" == 1 then
-  sumneko_path = sumneko_path .. "bin/lua-language-server"
+  lua_bin_path = lua_bin_path .. "bin/lua-language-server"
 elseif vim.fn.has "win32" == 1 then
-  sumneko_path = sumneko_path .. "lua-language-server/extension/server/bin/lua-language-server.exe"
+  lua_bin_path = lua_bin_path .. "lua-language-server/extension/server/bin/lua-language-server.exe"
 else
   return
 end
 
-require('lspconfig').sumneko_lua.setup({
+require('lspconfig').lua_ls.setup({
   on_attach    = function()
     base_config.keymap()
   end,
-  cmd          = { sumneko_path },
+  cmd          = { lua_bin_path },
   capabilities = capabilities,
   settings     = {
     Lua = {
