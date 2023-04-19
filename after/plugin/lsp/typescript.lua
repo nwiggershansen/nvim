badddce17cf11402a5be27dc76eaf1f9cedac853
KaptajnenGaming <Wiggers_@hotmail.com>
2023-04-19 20:21:48 +0200

    Move away from the keymap and use vim.keymap.set directly

--- after/plugin/lsp/typescript.lua
+++ after/plugin/lsp/typescript.lua
@@ -1,14 +1,15 @@
-local nnoremap = require('keymap').nnoremap
 local base_config = require('user.base_config')
 local typescript = require('typescript')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local opts = { remap = false }
 
 typescript.setup({
   capabilities = capabilities,
   server = {
     on_attach = function()
       base_config.keymap()
-      nnoremap('<F2>', '<cmd>TypescriptRenameFile<CR>')
+      vim.keymap.set('n', '<F2>', '<cmd>TypescriptRenameFile<CR>', opts)
     end
   }
 })
