e5cd90c4712700251f0e3b82e5b57bd0dbb593d7
KaptajnenGaming <Wiggers_@hotmail.com>
2023-04-27 19:22:11 +0200

    Add a temporary workaround since Omnisharp doesn't follow the LSP spec

--- after/plugin/lsp/omnisharp.lua
+++ after/plugin/lsp/omnisharp.lua
@@ -5,6 +5,15 @@ local mason_path = base_config.mason_path
 local omnisharp_path = mason_path .. "omnisharp/OmniSharp.exe"
 local opts = { remap = false, silent = true }
 
+
+capabilities = vim.tbl_deep_extend("force", capabilities, {
+  workspace = {
+    didChangeWatchedFiles = {
+      dynamicRegistration = true
+    }
+  }
+})
+
 local omnisharp_cmd = { omnisharp_path }
 table.insert(omnisharp_cmd, '--languageserver')
 
@@ -18,9 +27,20 @@ require('lspconfig').omnisharp.setup({
   handlers = {
     ["textDocument/definition"] = require('omnisharp_extended').handler
   },
-  on_attach = function()
+  on_attach = function(client, _)
     base_config.keymap()
     vim.keymap.set('n', '<C-f>', '<cmd>OmniSharpCodeFormat<CR>', opts)
     vim.keymap.set('n', '<C-k>', '<cmd>OmniSharpSignatureHelp<CR>', opts)
+
+   if client.name == 'omnisharp' then
+    local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+    for i, v in ipairs(tokenModifiers) do
+      tokenModifiers[i] = v:gsub(' ', '_')
+    end
+    local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+    for i, v in ipairs(tokenTypes) do
+      tokenTypes[i] = v:gsub(' ', '_')
+    end
   end
+end
 })
