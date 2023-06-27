a433e78f43529b5500a9c7b510b53e7ad93f8e4d
KaptajnenGaming <Wiggers_@hotmail.com>
2023-06-27 15:44:35 +0200

    Also allow greping through .-prefixed files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,4 +1,5 @@
 local status, telescope = pcall(require, "telescope")
+local grep_args = { '--hidden', '--glob', '!**/.git/*' }
 
 if not status then
   return
@@ -7,8 +8,18 @@ end
 telescope.setup {
   defaults = { file_ignore_patterns = { '.meta', '.unity' } },
   pickers = {
-   find_files = {
-     find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' }
-   }
+    find_files = {
+      find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' }
+    },
+    live_grep = {
+      additional_args = function(opts)
+        return grep_args
+      end
+    },
+    grep_string = {
+      additional_args = function(opts)
+        return grep_args
+      end
+    },
   }
 }
