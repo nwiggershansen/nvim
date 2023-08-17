5d2acd5eace49bc27a0dbc7695b367ca7af83af8
KaptajnenGaming <Wiggers_@hotmail.com>
2023-08-17 13:08:09 +0200

    Omnisharp 1.39.8 fixes the token type issue

--- after/plugin/lsp/omnisharp.lua
+++ after/plugin/lsp/omnisharp.lua
@@ -10,7 +10,7 @@ if vim.fn.has "mac" == 1 then
 elseif vim.fn.has "unix" == 1 then
   omnisharp_path = omnisharp_path .. "packages/omnisharp/omnisharp"
 elseif vim.fn.has "win32" == 1 then
-  omnisharp_path = omnisharp_path .. "omnisharp/OmniSharp.exe"
+  omnisharp_path = omnisharp_path .. "omnisharp/OmniSharp.cmd"
 else
   omnisharp_path = "omnisharp/omnisharp"
 end
@@ -44,90 +44,5 @@ require('lspconfig').omnisharp.setup({
     vim.keymap.set('n', '<C-k>', '<cmd>OmniSharpSignatureHelp<CR>', opts)
     vim.keymap.set('n', '<leader>gi', '<cmd>OmniSharpFindImplementations<CR>', opts)
     vim.keymap.set('n', '<leader>fu', '<cmd>OmniSharpFindUsages<CR>', opts)
-
-    -- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
-    client.server_capabilities.semanticTokensProvider = {
-      full = vim.empty_dict(),
-      legend = {
-        tokenModifiers = { "static_symbol" },
-        tokenTypes = {
-          "comment",
-          "excluded_code",
-          "identifier",
-          "keyword",
-          "keyword_control",
-          "number",
-          "operator",
-          "operator_overloaded",
-          "preprocessor_keyword",
-          "string",
-          "whitespace",
-          "text",
-          "static_symbol",
-          "preprocessor_text",
-          "punctuation",
-          "string_verbatim",
-          "string_escape_character",
-          "class_name",
-          "delegate_name",
-          "enum_name",
-          "interface_name",
-          "module_name",
-          "struct_name",
-          "type_parameter_name",
-          "field_name",
-          "enum_member_name",
-          "constant_name",
-          "local_name",
-          "parameter_name",
-          "method_name",
-          "extension_method_name",
-          "property_name",
-          "event_name",
-          "namespace_name",
-          "label_name",
-          "xml_doc_comment_attribute_name",
-          "xml_doc_comment_attribute_quotes",
-          "xml_doc_comment_attribute_value",
-          "xml_doc_comment_cdata_section",
-          "xml_doc_comment_comment",
-          "xml_doc_comment_delimiter",
-          "xml_doc_comment_entity_reference",
-          "xml_doc_comment_name",
-          "xml_doc_comment_processing_instruction",
-          "xml_doc_comment_text",
-          "xml_literal_attribute_name",
-          "xml_literal_attribute_quotes",
-          "xml_literal_attribute_value",
-          "xml_literal_cdata_section",
-          "xml_literal_comment",
-          "xml_literal_delimiter",
-          "xml_literal_embedded_expression",
-          "xml_literal_entity_reference",
-          "xml_literal_name",
-          "xml_literal_processing_instruction",
-          "xml_literal_text",
-          "regex_comment",
-          "regex_character_class",
-          "regex_anchor",
-          "regex_quantifier",
-          "regex_grouping",
-          "regex_alternation",
-          "regex_text",
-          "regex_self_escaped_character",
-          "regex_other_escape",
-        },
-      },
-      range = true,
-    }
-
---    local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
---    for i, v in ipairs(tokenModifiers) do
---      tokenModifiers[i] = v:gsub(' ', '_')
---    end
---    local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
---    for i, v in ipairs(tokenTypes) do
---      tokenTypes[i] = v:gsub(' ', '_')
---   end
   end
 })
