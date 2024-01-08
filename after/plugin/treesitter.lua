794a4088a1cdb1638099691591b3869d4c9efd65
nwiggershansen <niwh@redpill-linpro.com>
2024-01-08 09:48:02 +0100

    Also check file size

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,12 +4,17 @@ if not status then
   return
 end
 
+local MAX_FILE_SIZE = 100 * 1024
+
 nvim_treesitter.setup {
   ensure_installed = { 'javascript', 'typescript', 'tsx', 'bash', 'dockerfile', 'lua', 'bicep' },
   highlight = {
     enable = true,
     disable = function(_, bufnr)
-      return vim.api.nvim_buf_line_count(bufnr) > 50000
+      local buf_name = vim.api.nvim_buf_get_name(bufnr)
+      local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+
+      return file_size > MAX_FILE_SIZE or vim.api.nvim_buf_line_count(bufnr) > 50000
     end
   },
   sync_install = false,
