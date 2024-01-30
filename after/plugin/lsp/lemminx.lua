4d11f1824bc92f3693b7df57e9bcffd87b4c261d
nwiggershansen <niwh@redpill-linpro.com>
2024-01-30 15:34:58 +0100

    Fix dir for lemminx on ubuntu

--- after/plugin/lsp/lemminx.lua
+++ after/plugin/lsp/lemminx.lua
@@ -4,7 +4,7 @@ local lemminx_path = base_config.mason_path
 
 if vim.fn.has "mac" == 1 then
 elseif vim.fn.has "unix" == 1 then
-  lemminx_path = lemminx_path .. "lemminx/lemminx"
+  lemminx_path = lemminx_path .. "bin/lemminx"
 elseif vim.fn.has "win32" == 1 then
   lemminx_path = lemminx_path .. "lemminx/lemminx-win32.exe"
 else
