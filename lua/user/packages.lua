ddfd61ea364e5d4295559f622fa2ceccf321022c
nwiggershansen <Wiggers_@hotmail.com>
2024-05-08 10:59:56 +0200

    Update dependencies

--- lua/user/packages.lua
+++ lua/user/packages.lua
@@ -100,7 +100,7 @@ return require('lazy').setup({
 
   -- DAP (Debug Adapter Protocol)
   'mfussenegger/nvim-dap',
-  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
+  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap', "nvim-neotest/nvim-nio" } },
 
   -- Marker jumping
   'ThePrimeagen/harpoon',
