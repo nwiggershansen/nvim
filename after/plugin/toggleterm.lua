62ebf75070585d4a8ee8e952289a95e06417118e
nwiggershansen <Wiggers_@hotmail.com>
2024-05-13 18:19:55 +0200

    Simplify toggleterm

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,28 +1,19 @@
 local toggleterm = require('toggleterm')
-
+local shell
 if vim.fn.has "mac" == 1 then
 elseif vim.fn.has "unix" == 1 then
-  toggleterm.setup({
-    start_in_insert = true,
-    close_on_exit = true,
-    auto_scroll = true,
-    terminal_mappings = true,
-    shell = "bash"
-  })
-
-  vim.keymap.set({ 'n' }, '<leader>1', '<cmd>:1ToggleTerm direction=float<CR>')
-  vim.keymap.set({ 'n' }, '<leader>2', '<cmd>:2ToggleTerm<CR>')
-  vim.keymap.set({ 'n' }, '<leader>3', '<cmd>:3ToggleTerm<CR>')
+  shell = "bash"
 elseif vim.fn.has "win32" == 1 then
-  toggleterm.setup({
-    start_in_insert = true,
-    close_on_exit = true,
-    auto_scroll = true,
-    terminal_mappings = true,
-    shell = "cmd.exe"
-  })
-
-  vim.keymap.set({ 'n' }, '<leader>1', '<cmd>:1ToggleTerm direction=float<CR>')
-  vim.keymap.set({ 'n' }, '<leader>2', '<cmd>:2ToggleTerm<CR>')
-  vim.keymap.set({ 'n' }, '<leader>3', '<cmd>:3ToggleTerm<CR>')
+  shell = "cmd.exe"
 end
+toggleterm.setup({
+  start_in_insert = true,
+  close_on_exit = true,
+  auto_scroll = true,
+  terminal_mappings = true,
+  shell = shell
+})
+
+vim.keymap.set({ 'n' }, '<leader>1', '<cmd>:1ToggleTerm direction=float<CR>')
+vim.keymap.set({ 'n' }, '<leader>2', '<cmd>:2ToggleTerm<CR>')
+vim.keymap.set({ 'n' }, '<leader>3', '<cmd>:3ToggleTerm<CR>')
