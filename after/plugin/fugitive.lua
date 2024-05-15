230479c6bda27c279e89503016cc0307c042b12d
nwiggershansen <Wiggers_@hotmail.com>
2024-05-15 14:49:42 +0200

    Move fugitive setup

--- after/plugin/fugitive.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>zs", "<cmd>:Git<CR>", { desc = "Git Status" })
-vim.keymap.set("n", "<leader>za", "<cmd>:Git add .<CR>", { desc = "Git Add All" })
-vim.keymap.set("n", "<leader>zp", "<cmd>:Git push<CR>", { desc = "Git Push" })
