67c58b9976343ba38195e8b30d89f8069fed7672
nwiggershansen <Wiggers_@hotmail.com>
2024-05-25 08:25:22 +0200

    Update symbols library since the other one is archived

--- lua/custom/plugins/symbols-outline.lua
+++ lua/custom/plugins/symbols-outline.lua
@@ -1,11 +1,9 @@
 return {
   {
-    'simrat39/symbols-outline.nvim',
+    'hedyhli/outline.nvim',
     config = function()
-      local symbols_outline = require('symbols-outline')
-
-      symbols_outline.setup()
-      vim.keymap.set('n', '<leader>so', '<cmd>SymbolsOutline<CR>')
+      require('outline').setup()
+      vim.keymap.set('n', '<leader>so', '<cmd>Outline<CR>')
     end,
   },
 }
