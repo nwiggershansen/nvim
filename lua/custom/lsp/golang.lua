a660212bb1eb50b67d2035b3cf1b2e86c20f04fa
nwiggershansen <Wiggers_@hotmail.com>
2024-09-28 22:00:45 +0200

    Setup gopls for Windows

--- lua/custom/lsp/golang.lua
+++ lua/custom/lsp/golang.lua
@@ -2,11 +2,23 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local base_config = require('user.base_config')
 local util = require("lspconfig/util")
 
+local cmd = ""
+
+if vim.fn.has "mac" == 1 then
+elseif vim.fn.has "unix" == 1 then
+  cmd = "gopls"
+elseif vim.fn.has "win32" == 1 then
+  local mason_path = base_config.mason_path
+  cmd = mason_path .. "gopls/gopls.exe"
+else
+  cmd = "gopls"
+end
+
 require('lspconfig').gopls.setup({
   on_attach    = function(_, bufnr)
     base_config.keymap(bufnr)
   end,
-  cmd          = { "gopls" },
+  cmd          = { cmd },
   filetypes    = {
     "go",
     "gomod",
