2bcc5ff469ebd288d395e3eac46f4093134e3e23
nwiggershansen <Wiggers_@hotmail.com>
2024-12-10 08:15:48 +0100

    Don't format json filetypes by default

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -11,6 +11,10 @@ return {
         },
         format_on_save = {
           lsp_fallback = true, -- Enables fallback to `vim.lsp.buf.format` if no formatter is available
+          filter = function(_)
+            local filetype = vim.bo.filetype
+            return filetype ~= "json"
+          end,
         },
       })
     end,
