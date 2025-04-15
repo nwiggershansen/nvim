c1c2f177adc0d5d4872922ba2f917dae25ff21a5
nwiggershansen <Wiggers_@hotmail.com>
2025-04-15 08:35:09 +0200

    Formatting

--- lua/custom/lsp/lua_ls.lua
+++ lua/custom/lsp/lua_ls.lua
@@ -1,28 +1,28 @@
-local base_config = require('user.base_config')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local base_config = require("user.base_config")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lua_bin_path = base_config.mason_path
-if vim.fn.has "mac" == 1 then
-elseif vim.fn.has "unix" == 1 then
+if vim.fn.has("mac") == 1 then
+elseif vim.fn.has("unix") == 1 then
   lua_bin_path = lua_bin_path .. "bin/lua-language-server"
-elseif vim.fn.has "win32" == 1 then
+elseif vim.fn.has("win32") == 1 then
   lua_bin_path = lua_bin_path .. "lua-language-server/bin/lua-language-server.exe"
 else
   return
 end
 
-require('lspconfig').lua_ls.setup({
-  on_attach    = function(_, bufnr)
+require("lspconfig").lua_ls.setup({
+  on_attach = function(_, bufnr)
     base_config.keymap(bufnr)
   end,
-  cmd          = { lua_bin_path },
+  cmd = { lua_bin_path },
   capabilities = capabilities,
-  settings     = {
+  settings = {
     Lua = {
       hint = { enable = true },
       diagnostics = {
-        globals = { "vim" }
+        globals = { "vim" },
       },
-         }
-  }
+    },
+  },
 })
