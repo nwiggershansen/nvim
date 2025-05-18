eab37d10c0f0ebe9c14672a4dcf002cb6a6fd321
nwiggershansen <Wiggers_@hotmail.com>
2025-05-18 13:22:06 +0200

    Update conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -5,8 +5,8 @@ return {
       require("conform").setup({
         formatters_by_ft = {
           lua = { "stylua" },
-          javascript = { { "prettierd", "prettier" } },
-          typescript = { { "prettierd", "prettier" } },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
           go = { "gofmt" },
           py = { "ruff" },
         },
