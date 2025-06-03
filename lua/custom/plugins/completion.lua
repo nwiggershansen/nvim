53ccebfd0e29029375e2441072c6286ae740b6e9
nwiggershansen <Wiggers_@hotmail.com>
2025-06-03 19:22:57 +0200

    Working blink.cmp replacement

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -1,37 +1,82 @@
 return {
   {
-    "Saghen/blink.cmp",
+    "saghen/blink.cmp",
     dependencies = {
       "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
     },
+    version = "1.*",
+    lazy = false,
     opts = {
       keymap = {
-        preset = "enter",
-        ["<C-y>"] = { "select_and_accept" },
+        preset = "none",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<C-y>"] = { "accept", "fallback" },
+        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
       },
-      sources = {
-        providers = {
-          lsp = {
-            name = "lsp",
-            enabled = true,
-            module = "blink.cmp.sources.lsp",
-            kind = "LSP",
-            score_offset = 90,
-          },
-          snippets = {
-            name = "snippets",
-            enabled = true,
-            module = "blink.cmp.sources.snippets",
-            score_offset = 85,
+      completion = {
+        accept = {
+          auto_brackets = { enabled = true },
+        },
+        list = {
+          selection = { preselect = true, auto_insert = true },
+        },
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                ellipsis = false,
+                text = function(ctx)
+                  local lspkind = require("lspkind")
+                  return lspkind.symbolic(ctx.kind, { mode = "symbol" }) .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  return "BlinkCmpKind" .. ctx.kind
+                end,
+              },
+            },
+            columns = {
+              { "kind_icon" },
+              { "label", "label_description", gap = 1 },
+              { "source_name" },
+            },
           },
         },
+      },
+
+      sources = {
         default = { "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lsp = {
+            name = "LSP",
+            module = "blink.cmp.sources.lsp",
+          },
+          path = {
+            name = "path",
+            module = "blink.cmp.sources.path",
+            score_offset = 3,
+            opts = {
+              get_cwd = function()
+                return vim.fn.getcwd()
+              end,
+            },
+          },
+          snippets = {
+            name = "snip",
+            module = "blink.cmp.sources.snippets",
+            score_offset = -1,
+          },
+          buffer = {
+            name = "buf",
+            module = "blink.cmp.sources.buffer",
+            min_keyword_length = 5,
+          },
+        },
       },
-      cmdline = {
-        enabled = false,
-      },
-      fuzzy = { implementation = "lua" },
+      signature = { enabled = true },
     },
-    opts_extended = { "sources.default" },
   },
 }
