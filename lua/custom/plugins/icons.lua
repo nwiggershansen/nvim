c3378527289b9859a2f352f5868fdc84a5025d6b
nwiggershansen <Wiggers_@hotmail.com>
2025-06-22 09:49:10 +0200

    Specify some icons

--- lua/custom/plugins/icons.lua
+++ lua/custom/plugins/icons.lua
@@ -1,6 +1,13 @@
 return {
   "echasnovski/mini.icons",
-  opts = {},
+  opts = {
+    extension = {
+      csproj = { glyph = "󰪮", hl = "MiniIconsPurple" },
+      asset = { glyph = "󰆧", hl = "MiniIconsCyan" },
+      asmdef = { glyph = "󰈙", hl = "MiniIconsWhite" },
+      lock = { glyph = "󰌾", hl = "MiniIconsYellow" },
+    },
+  },
   init = function()
     require("mini.icons").mock_nvim_web_devicons()
   end,
