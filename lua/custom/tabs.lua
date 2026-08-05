da147309e769b63afdb09cbe5c36808778e02d55
nwiggershansen <wiggers_@hotmail.com>
2026-08-05 13:54:10 +0200

    Update packages, fix issues

--- lua/custom/tabs.lua
+++ lua/custom/tabs.lua
@@ -1,30 +1,15 @@
-local status, nvim_tree_events = pcall(require, "nvim-tree.events")
-
-if not status then
-  return
-end
-
-local bufferline_status, bufferline_api = pcall(require, "bufferline.api")
-
-if not bufferline_status then
-  return
-end
-
-local function get_tree_size()
-  return require("nvim-tree.view").View.width
-end
-
-nvim_tree_events.subscribe("TreeOpen", function()
-  bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe("Resize", function()
-  bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe("TreeClose", function()
-  bufferline_api.set_offset(0)
-end)
+require("bufferline").setup({
+  options = {
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "Explorer",
+        highlight = "Directory",
+        separator = true,
+      },
+    },
+  },
+})
 
 vim.keymap.set("n", "<A-,>", "<cmd>BufferPrevious<CR>")
 vim.keymap.set("n", "<A-.>", "<cmd>BufferNext<CR>")
